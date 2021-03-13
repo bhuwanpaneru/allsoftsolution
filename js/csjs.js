1eb8e0ccb32f8983dae1fc3898308050e5b972ad
Bhuwan Paneru <bhuwan@synchronisys.com>
2021-03-13 21:06:25 +0530

    seprated file

---
+++ js/csjs.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAmonJOiqVGAtKr4YJWBd8QL-a2vUSSMJA",
+    authDomain: "alltech-solution.firebaseapp.com",
+    projectId: "alltech-solution",
+    storageBucket: "alltech-solution.appspot.com",
+    messagingSenderId: "1081617902061",
+    appId: "1:1081617902061:web:0f7fac7f6dd326df151fe4",
+    measurementId: "G-T9F9BQZ7J5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
